30746c1ae239361d5a6ae9fd2b85bcd20c814b59
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-11-30 12:01:56 +0100

    Crea struttura header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,6 +4,21 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <header class="px-2 py-3 d-flex justify-content-between">
+    <h1>BOOLFLIX</h1>
+    <form action="" class="d-flex align-items-center p-2" @submit.prevent>
+      <input type="text" class="me-3 rounded-pill" />
+      <button class="rounded-pill">Cerca</button>
+    </form>
+  </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  background-color: black;
+  h1 {
+    color: darkred;
+  }
+}
+</style>
